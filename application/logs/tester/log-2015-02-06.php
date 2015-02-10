2b6d521c6076573b30951d1dedd67b3c8efb6110
cpooup <cpooup@gmail.com>
2015-02-10 13:08:52 +0700

    del log

--- application/logs/tester/log-2015-02-06.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-INFO  - 2015-02-06 05:32:59 --> 'tester' was delete customer 'asdf' on 'tester' site
-INFO  - 2015-02-06 05:33:02 --> 'tester' was delete customer 'asdf' on 'tester' site
-INFO  - 2015-02-06 05:33:04 --> 'tester' was delete customer 'asdf' on 'tester' site
-INFO  - 2015-02-06 05:33:08 --> 'tester' was delete customer 'bra' on 'tester' site
-INFO  - 2015-02-06 05:33:12 --> 'tester' was edit customer 'asdffasfad' on 'tester' site
-INFO  - 2015-02-06 05:33:16 --> 'tester' was edit customer 'asdfxxxx' on 'tester' site
